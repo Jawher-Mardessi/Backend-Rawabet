2b6eb27583f34b5ab12ebb280d6c3702aeac6b46
AmenallahNahali <Amenallah.Nahali@esprit.tn>
2026-03-26 02:54:36 +0100

    crud + auth

---
+++ src/main/java/org/example/rawabet/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package org.example.rawabet.config;
+
+import org.example.rawabet.security.JwtFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.http.HttpMethod;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**", "/users/add").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable())
+                // 🔥 AJOUT DU FILTER
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
--- src/main/java/org/example/rawabet/entities/User.java
+++ src/main/java/org/example/rawabet/entities/User.java
@@ -14,6 +14,7 @@ public class User {
     private Long id;
 
     private String nom;
+    @Column(unique = true)
     private String email;
     private String password;
 
